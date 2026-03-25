13b6bb600311ad6a4898a353f6065e1290963163
Lexihim <alexbello11e@gmail.com>
2026-03-25 09:39:16 +0100

    Add script.js with message and form submission functions

---
+++ script.js
@@ -0,0 +1,9 @@
+# Lexihim.auramode.github.io
+function showMessage() {
+    alert("Welcome to Velora Fashion House 😎🔥");
+}
+
+function submitForm() {
+    alert("Appointment Booked Successfully!");
+    return false;
+}
